913d803c6b79a71aac028062dde9fa92d64537c1
Nicolas Henry <nicolas.henry@cpnv.ch>
2019-03-27 17:59:00 +0100

    Add ngClass

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -10,11 +10,20 @@ import { $ } from 'protractor';
 export class StockItemComponent implements OnInit {
 
   public stock: Stock;
+  public stockClasses;
 
   constructor() { }
 
   ngOnInit() {
     this.stock = new Stock('Angular company', 'AC', 85, 80);
+    let diff = (this.stock.price / this.stock.previousPrice) - 1;
+    let largeChange = Math.abs(diff) > 0.01;
+    this.stockClasses = {
+      "positive": this.stock.isPositiveChange(),
+      "negative": !this.stock.isPositiveChange(),
+      "large-change": largeChange,
+      "small-change": !largeChange
+    };
   }
 
   toggleFavorite(event) {
